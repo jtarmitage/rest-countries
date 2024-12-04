ec797572205895ab3329c56d137ca5420b153565
James Armitage <james@jamesarmitage.co.uk>
2024-12-04 21:35:24 +0000

    Fetch specific country based on row click

--- src/App.tsx
+++ src/App.tsx
@@ -22,26 +22,65 @@ interface IRow {
 
 interface Country {
   name: {
-    common: string; // Common name of the country
-    official: string; // Official name of the country
-    nativeName: {
-      [key: string]: {
-        // Language code to native name mapping
-        common: string;
-        official: string;
-      };
-    };
+    common: string;
+    official: string;
+    nativeName: Record<string, { official: string; common: string }>;
   };
-  currencies: {
-    [key: string]: {
-      // Currency code to currency details
-      name: string;
-      symbol: string;
-    };
+  tld: string[];
+  cca2: string;
+  ccn3: string;
+  cca3: string;
+  cioc: string;
+  independent: boolean;
+  status: string;
+  unMember: boolean;
+  currencies: Record<string, { name: string; symbol: string }>;
+  idd: {
+    root: string;
+    suffixes: string[];
   };
-  languages: { [key: string]: string }; // Language code to language name mapping
-  flag: string; // Flag emoji
+  capital: string[];
+  altSpellings: string[];
+  region: string;
+  subregion: string;
+  languages: Record<string, string>;
+  translations: Record<string, { official: string; common: string }>;
+  latlng: [number, number];
+  landlocked: boolean;
+  borders: string[];
+  area: number;
+  demonyms: Record<string, { f: string; m: string }>;
+  flag: string;
+  maps: {
+    googleMaps: string;
+    openStreetMaps: string;
+  };
   population: number;
+  gini: Record<string, number>;
+  fifa: string;
+  car: {
+    signs: string[];
+    side: string;
+  };
+  timezones: string[];
+  continents: string[];
+  flags: {
+    png: string;
+    svg: string;
+    alt: string;
+  };
+  coatOfArms: {
+    png: string;
+    svg: string;
+  };
+  startOfWeek: string;
+  capitalInfo: {
+    latlng: [number, number];
+  };
+  postalCode: {
+    format: string;
+    regex: string;
+  };
 }
 
 const transformCountryData = (country: Country) => {
@@ -57,7 +96,11 @@ const transformCountryData = (country: Country) => {
 };
 
 // Create new Grid component
-const CountriesGrid = () => {
+const CountriesGrid = ({
+  selectedCountry,
+}: {
+  selectedCountry: (countryName: string) => void;
+}) => {
   // State to store loading status
   const [loading, setLoading] = useState<boolean>(true);
   // State to store any potential error
@@ -87,21 +130,13 @@ const CountriesGrid = () => {
   }, []);
 
   // Row Data: The data to be displayed.
-  const [rowData, setRowData] = useState<IRow[]>([
-    {
-      name: "JamesCountry",
-      flag: "test",
-      population: 1234,
-      languages: "English",
-      currencies: "Pounds",
-    },
-  ]);
+  const [rowData, setRowData] = useState<IRow[]>([]);
 
   // Column Definitions: Defines & controls grid columns.
   const [colDefs, setColDefs] = useState<ColDef<IRow>[]>([
     { field: "name" },
     { field: "flag" },
-    { field: "population" },
+    { field: "population", filter: "agNumberColumnFilter" },
     { field: "languages" },
     { field: "currencies" },
   ]);
@@ -121,17 +156,60 @@ const CountriesGrid = () => {
         rowData={rowData}
         columnDefs={colDefs}
         defaultColDef={defaultColDef}
+        onRowClicked={(e) => selectedCountry(e.node.data.name)}
       />
     </div>
   );
 };
 
+const CountryInfo = ({ countryName }: { countryName: string }) => {
+  const [countryData, setCountryData] = useState<Country[]>([]);
+  // State to store loading status
+  const [loading, setLoading] = useState<boolean>(true);
+  // State to store any potential error
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchCountry = async () => {
+      if (countryName.length > 0) {
+        try {
+          const response = await fetch(
+            "https://restcountries.com/v3.1/name/" + countryName
+          );
+          if (!response.ok) {
+            throw new Error("Failed to fetch countries data");
+          }
+          const data: Country[] = await response.json();
+          setCountryData(data);
+        } catch (error) {
+          setError(
+            error instanceof Error ? error.message : "An unknown error occurred"
+          ); // Ensure proper error handling
+        } finally {
+          setLoading(false); // Set loading to false once the request is finished
+        }
+      }
+    };
+
+    fetchCountry();
+  }, [countryName]);
+
+  return countryData.length > 0 ? (
+    <p>{countryData[0]?.name.common}</p>
+  ) : (
+    <p>Click on a country to view full details.</p>
+  );
+};
+
 function App() {
+  const [country, setCountry] = useState<string>("");
+
   return (
     <div className="App">
       <h1>Countries App</h1>
 
-      <CountriesGrid />
+      <CountriesGrid selectedCountry={(country) => setCountry(country)} />
+      <CountryInfo countryName={country} />
     </div>
   );
 }
