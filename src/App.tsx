06d6a40f4f0326f320bcbf906a5d6f08f6b1e8b0
James Armitage <james@jamesarmitage.co.uk>
2024-12-04 19:24:41 +0000

    Update table with relevant columns

--- src/App.tsx
+++ src/App.tsx
@@ -13,34 +13,38 @@ ModuleRegistry.registerModules([ClientSideRowModelModule]);
 
 // Row Data Interface
 interface IRow {
-  make: string;
-  model: string;
-  price: number;
-  electric: boolean;
+  name: string;
+  flag: string;
+  population: number;
+  languages: string;
+  currencies: string;
 }
 
 // Create new GridExample component
 const GridExample = () => {
   // Row Data: The data to be displayed.
   const [rowData, setRowData] = useState<IRow[]>([
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-    { make: "Mercedes", model: "EQA", price: 48890, electric: true },
-    { make: "Fiat", model: "500", price: 15774, electric: false },
-    { make: "Nissan", model: "Juke", price: 20675, electric: false },
+    {
+      name: "JamesCountry",
+      flag: "test",
+      population: 1234,
+      languages: "English",
+      currencies: "Pounds",
+    },
   ]);
 
   // Column Definitions: Defines & controls grid columns.
   const [colDefs, setColDefs] = useState<ColDef<IRow>[]>([
-    { field: "make" },
-    { field: "model" },
-    { field: "price" },
-    { field: "electric" },
+    { field: "name" },
+    { field: "flag" },
+    { field: "population" },
+    { field: "languages" },
+    { field: "currencies" },
   ]);
 
   const defaultColDef: ColDef = {
     flex: 1,
+    filter: "agTextColumnFilter",
   };
 
   // Container: Defines the grid's theme & dimensions.
@@ -61,7 +65,7 @@ const GridExample = () => {
 function App() {
   return (
     <div className="App">
-      <h1>React Countries</h1>
+      <h1>Countries App</h1>
 
       <GridExample />
     </div>
