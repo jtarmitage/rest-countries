e2c9dfee9fd4de2f6631a5b21052e8e4a5a48e9b
James Armitage <james@jamesarmitage.co.uk>
2024-12-04 17:11:47 +0000

    Setting up simple Ag-Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,71 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
+// Theme
+import { ColDef, ModuleRegistry } from "@ag-grid-community/core";
+import { AgGridReact } from "@ag-grid-community/react";
+// React Grid Logic
+import "@ag-grid-community/styles/ag-grid.css";
+// Core CSS
+import "@ag-grid-community/styles/ag-theme-quartz.css";
+
+ModuleRegistry.registerModules([ClientSideRowModelModule]);
+
+// Row Data Interface
+interface IRow {
+  make: string;
+  model: string;
+  price: number;
+  electric: boolean;
+}
+
+// Create new GridExample component
+const GridExample = () => {
+  // Row Data: The data to be displayed.
+  const [rowData, setRowData] = useState<IRow[]>([
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+    { make: "Mercedes", model: "EQA", price: 48890, electric: true },
+    { make: "Fiat", model: "500", price: 15774, electric: false },
+    { make: "Nissan", model: "Juke", price: 20675, electric: false },
+  ]);
+
+  // Column Definitions: Defines & controls grid columns.
+  const [colDefs, setColDefs] = useState<ColDef<IRow>[]>([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" },
+  ]);
+
+  const defaultColDef: ColDef = {
+    flex: 1,
+  };
+
+  // Container: Defines the grid's theme & dimensions.
+  return (
+    <div
+      className={"ag-theme-quartz"}
+      style={{ width: "100%", height: "500px" }}
+    >
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={colDefs}
+        defaultColDef={defaultColDef}
+      />
+    </div>
+  );
+};
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>React Countries</h1>
+
+      <GridExample />
+    </div>
+  );
 }
 
 export default App;
